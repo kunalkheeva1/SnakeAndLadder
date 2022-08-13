df3608c1750df668b9393c516475471a3163ccab
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-12 21:03:44 -0700

    coded the method which returns the minimum number of moves to reach the last cell of the ludo

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Pair{
     int cell;
     int level;
@@ -11,6 +14,49 @@ class Pair{
 
 public class SnakeAndLadder {
 
+    // n denotes number of snakes and ladders,
+    // int [] arr contains their subsequent combinations
+    static int snakeAndLadder(int n, int []arr){
+        int[] connection = new int[31];
+        for(int i=0; i<31; i++){
+            connection[i]= -1;
+        }
+
+        for(int i=0; i<2*n; i+=2){
+            connection[arr[i]] = arr[i+1];
+        }
+        Queue<Pair> q = new LinkedList<>();
+        Pair p = new Pair(1,0);
+        q.add(p);
+
+        while(!q.isEmpty()){
+            Pair qp = q.remove();
+            int currCell = qp.cell;
+            int currLevel = qp.level;
+
+            if(currCell == 30){
+                return currLevel;
+            }
+            for(int i=currCell+1; i<currCell+6; i++){
+
+                if(currCell>30){
+                    break;
+                }
+                if(connection[i]==-1){
+                    Pair p1 = new Pair(i, currLevel+1);
+                    q.add(p1);
+                }
+                else{
+                    Pair p2 = new Pair(connection[i],currLevel+1 );
+                    q.add(p2);
+                }
+
+
+            }
+
+
+        }return -1;
+    }
 
     public static void main(String[] args) {
 
