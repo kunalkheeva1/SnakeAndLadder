821750ffd1bea297f1eff98d761b0ba774a98bd1
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-12 21:15:04 -0700

    confusive comments added, so please try to visualize it more carefully, its so easy, just get the feel of the problem

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,6 +1,7 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
+//pair class to keep the track of the cell and the number of moves(level)
 class Pair{
     int cell;
     int level;
@@ -17,6 +18,8 @@ public class SnakeAndLadder {
     // n denotes number of snakes and ladders,
     // int [] arr contains their subsequent combinations
     static int snakeAndLadder(int n, int []arr){
+        //get a connection array and store the next cell number reached as value and previous cell number as index
+        // if there is no such movement(like no ladder or snake) then mark it as -1
         int[] connection = new int[31];
         for(int i=0; i<31; i++){
             connection[i]= -1;
@@ -25,6 +28,8 @@ public class SnakeAndLadder {
         for(int i=0; i<2*n; i+=2){
             connection[arr[i]] = arr[i+1];
         }
+
+        //start a pair from first cell and with no movement
         Queue<Pair> q = new LinkedList<>();
         Pair p = new Pair(1,0);
         q.add(p);
@@ -34,9 +39,13 @@ public class SnakeAndLadder {
             int currCell = qp.cell;
             int currLevel = qp.level;
 
+            //if my final cell reached then return its level(which is basically moves)
             if(currCell == 30){
                 return currLevel;
             }
+            //otherwise check the next 6 moves of the dice, if its getting out of bound then break
+            //else if no drastic move possible then store the index(which is current cell +move) and add 1 to count(level)
+            //else just add the future cell number and the level carried forward
             for(int i=currCell+1; i<currCell+6; i++){
 
                 if(currCell>30){
